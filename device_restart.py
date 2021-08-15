5e4462ca41b5ae9e143bffb19ec571880fba7bcb
Mohammed Shafeeq M <mshafeeqmsmc@gmail.com>
2021-08-15 09:49:13 +0000

    Device restart and status file added

---
+++ device_restart.py
@@ -0,0 +1,6 @@
+from boltiot import Bolt
+api_key = "e9a57e96-679d-4ab3-988b-e7a95ad54702"
+device_id  = "BOLT8022282"
+mybolt = Bolt(api_key, device_id)
+response = mybolt.restart()
+print (response)
